85fe793beba149f762d7752016803ee46dae85b9
KushMyDaily <kushan@itsmydaily.com>
2024-06-28 01:11:46 +0530

    changed google redirect

--- controllers/google.controller.js
+++ controllers/google.controller.js
@@ -10,7 +10,7 @@ const googleService = new GoogleService()
 const oAuth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    `${process.env.URL_LOCAL}/redirect`
+    `${process.env.URL_LOCAL}/api/redirect`
 )
 
 const calendar = google.calendar({
