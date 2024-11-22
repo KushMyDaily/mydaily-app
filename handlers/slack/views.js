492417c7c51ac47bb1fa026cad2ac5205ac65257
kushan <kushan@itsmydaily.com>
2024-11-22 13:10:47 +0530

    change-survey-view

--- handlers/slack/views.js
+++ handlers/slack/views.js
@@ -1,9 +1,9 @@
 const db = require('../../models')
 const { WebClient } = require('@slack/web-api')
 const SlackService = require('../../service/slack.service')
+const { Op } = require('sequelize')
 
 const {
-    survey: Survey,
     user: User,
     surveyAnswer: SurveyAnswer,
     workspaceUser: WorkspaceUser,
@@ -17,67 +17,87 @@ module.exports = (slackApp) => {
         console.log(body)
         console.log(payload)
 
+        const user = await User.findOne({
+            where: {
+                [Op.or]: [
+                    {
+                        workspaceUserIds: {
+                            [Op.contains]: [body?.user?.id], // For array cases
+                        },
+                    },
+                    {
+                        workspaceUserIds: body?.user?.id, // For single value cases
+                    },
+                ],
+            },
+        })
+
+        if (user === null) {
+            console.log('>> Error while finding user: ')
+            return
+        }
+
+        const surveyId = payload?.private_metadata
+        if (surveyId === undefined) {
+            console.log('>> Error: No survey ID is provided')
+            return
+        }
+
         const answer01 =
             payload?.state?.values?.q01?.q01?.selected_option?.value
         const answer02 =
             payload?.state?.values?.q02?.q02?.selected_option?.value
-        const surveyId = payload?.private_metadata
-
-        let user
-        try {
-            user = await WorkspaceUser.findOne({
-                where: { userId: body?.user?.id },
-            })
-        } catch (error) {
-            console.log('>> Error while finding user: ', err)
+        if (answer01 === undefined || answer02 === undefined) {
+            console.log('>> Error: No answer is provided')
+            return
         }
 
-        if (surveyId && user && answer01 && answer02) {
-            const answers = [Number(answer01), Number(answer02)]
-            try {
-                await SurveyAnswer.create({
-                    answers: JSON.stringify(answers),
-                    surveyId: Number(surveyId),
-                    userId: user.id,
-                })
-            } catch (error) {
-                console.log('>> Error saving survey answers:', error)
-            }
+        const answers = [Number(answer01), Number(answer02)]
+        try {
+            await SurveyAnswer.create({
+                answers: JSON.stringify(answers),
+                surveyId: Number(surveyId),
+                userId: user.id,
+            })
+        } catch (error) {
+            console.log('>> Error saving survey answers:', error)
+        }
 
-            try {
-                let token
-                await slackService
-                    .authorize(user?.teamId)
-                    .then((res) => {
-                        token = res.botToken
-                    })
-                    .catch((err) => {
-                        throw new Error('Failed authorize token', err)
-                    })
-                await webClient.chat.update({
-                    token: token,
-                    channel: user?.channelId,
-                    ts: user?.postedTimestamp,
-                    blocks: [
-                        {
-                            type: 'header',
-                            text: {
-                                type: 'plain_text',
-                                text: 'Successfully submitted your answer! :v:',
-                                emoji: true,
-                            },
-                        },
-                    ],
+        const workspaceUser = await WorkspaceUser.findOne({
+            where: { userId: body?.user?.id },
+        })
+        if (workspaceUser === null) {
+            console.log('>> Error while finding workspace user: ')
+            return
+        }
+
+        try {
+            let token
+            await slackService
+                .authorize(workspaceUser?.teamId)
+                .then((res) => {
+                    token = res.botToken
                 })
-            } catch (error) {
-                console.error('>> Error updating message:', error)
-            }
-        } else {
-            console.log('>> Error saving survey answers:')
-            console.log('survey:', surveyId)
-            console.log('user:', user.id)
-            console.log('answer01:', answer01)
-            console.log('answer02:', answer02)
+                .catch((err) => {
+                    throw new Error('Failed authorize token', err)
+                })
+            await webClient.chat.update({
+                token: token,
+                channel: workspaceUser?.channelId,
+                ts: workspaceUser?.postedTimestamp,
+                blocks: [
+                    {
+                        type: 'header',
+                        text: {
+                            type: 'plain_text',
+                            text: 'Successfully submitted your answer! :v:',
+                            emoji: true,
+                        },
+                    },
+                ],
+            })
+        } catch (error) {
+            console.error('>> Error updating message:', error)
         }
     })
 }
