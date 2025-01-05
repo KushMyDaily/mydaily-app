fadab25c14798a62e1dd58ce6f7c14b86e0afed6
kushan <kushan@itsmydaily.com>
2025-01-05 10:06:43 +0530

    feature: hover information popover

--- client/src/comps/CircularProgressCard/index.js
+++ client/src/comps/CircularProgressCard/index.js
@@ -7,9 +7,12 @@ import {
   CircularProgress,
   CircularProgressLabel,
   Flex,
+  Popover,
+  PopoverHeader,
+  PopoverBody,
 } from "@chakra-ui/react";
 import React from "react";
-import { FiArrowDownRight, FiArrowUpRight } from "react-icons/fi";
+import { FiArrowDownRight, FiArrowUpRight, FiInfo } from "react-icons/fi";
 import styles from "./circularProgressCard.module.css";
 
 const CircularProgressCard = ({
@@ -19,13 +22,36 @@ const CircularProgressCard = ({
   statusText,
   color,
   bgColor,
+  isHelperText = false,
+  helperTitle,
+  helperText,
 }) => {
   return (
     <Card className={styles.cardWrap}>
       <CardHeader padding="0" marginBottom="15px">
-        <Text fontSize="22px" fontWeight="500" lineHeight="30px">
-          {heading}
-        </Text>
+        <Flex alignItems="center" justifyContent="space-between">
+          <Text fontSize="22px" fontWeight="500" lineHeight="30px">
+            {heading}
+          </Text>
+          {isHelperText && (
+            <Popover placement="left" strategy="fixed" boxShadow={"none"}>
+              <div className={styles.tooltipContainer}>
+                <FiInfo fontSize="24px" />
+                <Box className={styles.tooltip}>
+                  <PopoverHeader fontWeight="semibold">
+                    {helperTitle}
+                  </PopoverHeader>
+                  <PopoverBody>
+                    <Text
+                      className={styles.tooltipContent}
+                      dangerouslySetInnerHTML={{ __html: helperText }}
+                    ></Text>
+                  </PopoverBody>
+                </Box>
+              </div>
+            </Popover>
+          )}
+        </Flex>
       </CardHeader>
       <CardBody padding={0}>
         <Box textAlign={"center"}>
