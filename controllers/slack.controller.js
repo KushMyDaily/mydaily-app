87d84d08e123b5edf56b0c309ca5b40125b090de
kushan <kushan@itsmydaily.com>
2025-01-12 15:03:58 +0530

    bugfix:slack-controller-improvement

--- controllers/slack.controller.js
+++ controllers/slack.controller.js
@@ -19,61 +19,11 @@ exports.runSurvey = async () => {
         const slackOauth = await SlackOAuthController.findAll()
 
         if (slackOauth) {
-            slackOauth.forEach(async (oauth) => {
-                let token
-                await slackService
-                    .authorize(oauth.teamId)
-                    .then((res) => {
-                        if (oauth.tokenType === 'bot') {
-                            token = res.botToken
-                        } else if (oauth.tokenType === 'user') {
-                            token = res.userToken
-                        }
-                    })
-                    .catch((err) => {
-                        throw new Error('Failed authorize token', err)
-                    })
-
-                if (oauth) {
-                    const filtedWorkspaceUsers = await Promise.all(
-                        oauth.workspace_users.map(async (workspaceUser) => {
-                            const rows = await User.findAll({
-                                where: {
-                                    [Op.or]: [
-                                        { workspaceUserIds: workspaceUser.id }, // Check for equality with number
-                                        {
-                                            // Check if the column is not null and not empty
-                                            [Op.and]: [
-                                                {
-                                                    workspaceUserIds: {
-                                                        [Op.ne]: null,
-                                                    },
-                                                }, // Ensure it's not null
-                                                {
-                                                    workspaceUserIds: {
-                                                        [Op.ne]: '',
-                                                    },
-                                                }, // Ensure it's not empty
-                                                Sequelize.where(
-                                                    Sequelize.fn(
-                                                        'JSON_CONTAINS',
-                                                        Sequelize.col(
-                                                            'workspaceUserIds'
-                                                        ),
-                                                        JSON.stringify([
-                                                            workspaceUser.id,
-                                                        ])
-                                                    ),
-                                                    true
-                                                ),
-                                            ],
-                                        },
-                                    ],
-                                },
-                            })
-                            // Return the workspaceUser if rows.length > 0, otherwise return null
-                            return rows.length > 0 ? workspaceUser : null
-                        })
+            for (const oauth of slackOauth) {
+                try {
+                    let token
+                    const authResult = await slackService.authorize(
+                        oauth.teamId
                     )
 
                     for (const user of filtedWorkspaceUsers.filter(
@@ -215,10 +165,10 @@ exports.runSurvey = async () => {
                         }
                     }
                 }
-            })
+            }
         }
     } catch (err) {
-        console.log(err)
+        console.error('General error:', err)
     }
 }
 
